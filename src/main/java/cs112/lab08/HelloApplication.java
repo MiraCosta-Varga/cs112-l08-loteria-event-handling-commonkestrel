1b8d69346869ca7787f4b1f652d585b264b5ff2c
commonkestrel <jettb6@gmail.com>
2024-11-10 22:09:50 -0800

    finish lab 08 :D

--- src/main/java/cs112/lab08/HelloApplication.java
+++ src/main/java/cs112/lab08/HelloApplication.java
@@ -1,31 +1,111 @@
 package cs112.lab08;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
     //CONSTANTS
 
     //array of LoteriaCards to use for game:
     private static final LoteriaCard[] LOTERIA_CARDS = {
-            new LoteriaCard("Las matematicas", "1.png", 1),
-            new LoteriaCard("Las ciencias", "2.png", 2),
-            new LoteriaCard("La Tecnología", "8.png", 8),
-            new LoteriaCard("La ingeniería", "9.png", 9),
+        new LoteriaCard("Las matematicas", "1.png", 1),
+        new LoteriaCard("Las ciencias", "2.png", 2),
+        new LoteriaCard("La Tecnología", "8.png", 8),
+        new LoteriaCard("La ingeniería", "9.png", 9),
     };
 
+    // INSTANCE VARIABLES
+    ImageView cardImageView;
+    Text messageLabel;
+    Button drawCardButton;
+    ProgressBar cardProgress;
+    int cardIndex = -1;
 
     @Override
     public void start(Stage stage) throws IOException {
-        //removed FXML code, fill this in with components, scene, stage, etc.
+        VBox layout = new VBox();
+
+        Label titleLabel = new Label("EChALE STEM Loteria");
+        titleLabel.setStyle("-fx-font: 20 arial;");
+
+        cardImageView = new ImageView(loadLogo());
+        cardImageView.fitWidthProperty().bind(new ReadOnlyDoubleWrapper(275.0));
+        cardImageView.setPreserveRatio(true);
+        cardImageView.setSmooth(true);
+
+        messageLabel = new Text("");
+        messageLabel.setWrappingWidth(300.0);
+        messageLabel.setTextAlignment(TextAlignment.CENTER);
+
+        drawCardButton = new Button();
+        drawCardButton.setText("Draw Card");
+        drawCardButton.setStyle("-fx-background-color: #1E90FF; -fx-text-fill: white; -fx-background-radius: 10px; -fx-padding: 10px;");
+        drawCardButton.setOnAction(e -> {
+            cardIndex++;
+            cardProgress.setProgress(((double) cardIndex + 1) / LOTERIA_CARDS.length);
+
+            if (cardIndex >= LOTERIA_CARDS.length) {
+                cardImageView.setImage(loadLogo());
+                drawCardButton.setDisable(true);
+                cardProgress.setStyle("-fx-accent: red");
+                messageLabel.setText("GAME OVER. No more cards! Exit and run program again to reset ^_^");
+            } else {
+                LoteriaCard currentCard = LOTERIA_CARDS[cardIndex];
+                messageLabel.setText(currentCard.getCardName());
+                cardImageView.setImage(currentCard.getImage());
+            }
+        });
+
+        cardProgress = new ProgressBar();
+        cardProgress.setProgress(0.0);
+        cardProgress.setPadding(new Insets(20.0));
+        cardProgress.setMinWidth(200.0);
+
+        layout.getChildren().addAll(titleLabel, cardImageView, messageLabel, drawCardButton, cardProgress);
+        layout.setAlignment(Pos.BASELINE_CENTER);
+
+        Scene scene = new Scene(layout, 350, 500);
+        stage.setScene(scene);
+        stage.setTitle("EChALE STEM");
+        stage.show();
+    }
+
+    private Image loadLogo() {
+        FileInputStream input = null;
+		try {
+			input = new FileInputStream("./src/main/resources/images/0.png");
+		} catch (FileNotFoundException e) {
+			//e.printStackTrace();
+			System.err.println("ERROR: could not open file.");
+			System.exit(0);
+		}
+		return new Image(input);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/cs112/lab08/HelloController.java
+++ src/main/java/cs112/lab08/HelloController.java
@@ -1,14 +1,21 @@
 package cs112.lab08;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HelloController implements EventHandler<ActionEvent> {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        
     }
 }
